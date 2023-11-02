b459d0a9ed9823aa0621c3512b5384650488a3e0
Alexander Jason <alexanderjason526@gmail.com>
2023-11-02 17:33:32 +0700

    setup

---
+++ src/global.tsx
@@ -0,0 +1,13 @@
+export const colors = {
+    black: 'hsl(0, 0%, 8%)',
+    navBlack: 'hsl(0, 0%, 4%)',
+    white: '#ffffff',
+    teal: '#118A7E',
+    mint: '#3BAEA0',
+    blackLighter: 'hsl(0, 0%, 20%)',
+    pureBlack: '#000000',
+    disabled: '#c2c2c2',
+    red: '#D71414',
+    grey: '#e5e5e5',
+    yellow: '#C4A536'
+};
---
+++ src/index.tsx
@@ -0,0 +1,30 @@
+import * as React from "react"
+import * as ReactDOM from "react-dom/client"
+import { App } from "./App"
+// import reportWebVitals from "./reportWebVitals"
+// import * as serviceWorker from "./serviceWorker"
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import { ChakraProvider, ColorModeScript, extendTheme, ThemeConfig  } from '@chakra-ui/react'
+import './index.css'
+
+const theme: ThemeConfig = extendTheme({
+  fonts: {
+    body: `'Poppins', sans-serif`,
+  },
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+})
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <Router>
+    <React.StrictMode>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.initialColorMode} />
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>
+  </Router>
+)
+
