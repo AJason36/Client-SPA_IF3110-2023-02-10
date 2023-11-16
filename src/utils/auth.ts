01d06db6b816439e6493f84409f15b1b95dc646f
Alexander Jason <alexanderjason526@gmail.com>
2023-11-16 15:57:06 +0700

    feat: add utils

---
+++ src/utils/auth.ts
@@ -0,0 +1,57 @@
+import { jwtDecode } from "jwt-decode";
+
+export type Payload = {
+  username: string;
+  email: string;
+  fullName: string;
+  role: string;
+};
+
+function setAuthToken(token: string) {
+  localStorage.setItem("token", token);
+}
+
+function getAuthData(): Payload {
+  const tokentest = localStorage.getItem("token");
+  try {
+    const decoded: Payload = jwtDecode(tokentest!);
+    return decoded;
+  } catch (err) {
+    console.log(err)
+    return {
+      username: "",
+      email: "",
+      fullName: "",
+      role: "",
+    };
+  }
+}
+
+function logout() {
+  localStorage.removeItem("token");
+}
+
+function getUsername(): string {
+  return getAuthData().username;
+}
+
+function getEmail(): string {
+  return getAuthData().email;
+}
+
+function getFullName(): string {
+  return getAuthData().fullName;
+}
+
+function getRole(): string {
+  return getAuthData().role.toString();
+}
+export {
+  setAuthToken,
+  getAuthData,
+  getUsername,
+  getEmail,
+  getFullName,
+  getRole,
+  logout,
+};
