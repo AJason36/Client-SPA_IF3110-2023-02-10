f07f9c3f6180ded9262988bcb30a4c19cebabcd3
Alexander Jason <alexanderjason526@gmail.com>
2023-11-16 15:57:32 +0700

    feat: add routing rest props

---
+++ src/config/config.tsx
@@ -0,0 +1,5 @@
+const config = {
+    "REST_API_URL": "http://localhost:8040/api",
+  }
+  
+  export default config;
---
+++ src/types/route.d.ts
@@ -0,0 +1,8 @@
+type RouteProps = {
+  redirectPath?: string;
+    unauthPath?: string;
+    children: JSX.Element;
+    routeType: boolean;
+  }
+  
+export { RouteProps }
