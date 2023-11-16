5163ef9434e9858b1abc14b2d53469ef55bdce61
Alexander Jason <alexanderjason526@gmail.com>
2023-11-16 21:34:39 +0700

    feat: integrate detail book page

--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -1,44 +1,146 @@
-// BookDetail.tsx
-import React, { useState } from 'react';
-import "./BookDetail.css"
+import { FunctionComponent, useEffect,useState } from "react";
+import {
+  Flex,
+  Input,
+  Button,
+  Heading,
+  NumberInput,
+  NumberInputField,
+  Textarea,
+  Center,
+  FormControl,
+  FormLabel,
+  Text,
+  Link
+} from "@chakra-ui/react";
+import Navbar from "../components/Navbar";
+import { useLocation,useNavigate, Link as RouterLink } from "react-router-dom";
+import { axiosConfig } from "../utils/axios";
+import { getRole, getUsername } from "../utils/auth";
+import axios from "axios";
+import config from "../config/config";
 
-interface BookInfo {
-  title: string;
-  publisher: string;
-  year: number;
-  genre: string;
-  text: string;
-  audio: string;
-}
-interface Details{
-    details:BookInfo;
-}
-const BookDetail= ({ details }: Details) => {
-  const [book, setBook] = useState<BookInfo>({
-    title: 'Cerita Sang Pembelajar',
-    publisher: 'John Doe',
-    year: 2017,
-    genre: 'Fiksi',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    audio: 'sample_audio.mp3',
-  });
-
-  const playAudio = () => {
-    const audioElement = new Audio(details.audio);
-    audioElement.play();
-  };
+const BookDetail: FunctionComponent = () => {
+    type PremiumBook = {
+        title: string;
+        genre: string;
+        year: number;
+        content: string;
+        duration: number;
+        audioPath: string;
+    }
+  const location = useLocation();
+  const { book_id } = location.state;
+  const [book,setBook] = useState<PremiumBook>();
+  const axiosInstance = axios.create(axiosConfig());
+  const username = getUsername();
+  const navigate = useNavigate();
+  useEffect(() => {
+    axiosInstance.get(`${config.REST_API_URL}/premium-details/${book_id}`).then((res) => {
+      const bookData: PremiumBook = {
+        title: res.data.book.title,
+        genre: res.data.book.genre,
+        year: res.data.book.year,
+        content: res.data.book.content,
+        duration: res.data.book.duration,
+        audioPath: res.data.book.audioPath,
+      }
+      setBook(bookData);
+     })
+  },[])
 
   return (
-    <div>
-        
-      <h1>{details.title}</h1>
-      <p>Publisher: {details.publisher}</p>
-      <p>Year: {details.year}</p>
-      <p>Genre: {details.genre}</p>
-      <p>Text: {details.text}</p>
-      <button onClick={playAudio}>Play Audio</button>
-    </div>
+    <>
+      <Navbar children={undefined} />
+      <Center>
+        <Flex
+          w={{ lg: "80vw" }}
+          padding={"2rem"}
+          flexDirection={"column"}
+        >
+          {getRole() === "author" ?
+            <Link href="/../my-book" color="blue.500" mb={4}>
+              Back
+            </Link> :
+            <Link href="/../collection" color="blue.500" mb={4}>
+            Back
+          </Link>
+            }
+          <Center>
+            <Heading
+              as="h2"
+              mb={4}
+            >
+              Book Details</Heading>
+          </Center>
+          
+          <Flex border='2px' py={5} borderColor={"teal"} borderRadius={10} alignContent={"center"} justifyContent={"center"}>
+            <FormControl
+              className="form-content flex-column"
+              border={4}
+              borderColor={"teal"}
+              w={"80%"}
+            >
+              <FormControl id="book_id">
+              <FormLabel className="form-label" htmlFor="title">
+                Title:
+              </FormLabel>
+                <Text mb={4}>
+                  {book?.title}
+              </Text>
+              </FormControl>
+
+              <FormControl id="year">
+              <FormLabel className="form-label" htmlFor="year">
+                Year Published
+              </FormLabel>
+                <Text mb={4}>
+                  {book?.year}
+              </Text>
+              </FormControl>
+
+              <FormControl id="detail">
+              <FormLabel className="form-label" htmlFor="summary">
+                Detail
+              </FormLabel>
+                <Text mb={4}>
+                  {book?.content}
+                </Text>
+              </FormControl>
+
+              <FormControl id="genre">
+              <FormLabel className="form-label" htmlFor="genre">
+                Genre
+              </FormLabel>
+                <Text mb={4}> 
+                  {book?.genre}
+              </Text>
+              </FormControl>
+
+              <FormControl id="duration">
+              <FormLabel className="form-label" htmlFor="duration">
+                Duration
+              </FormLabel>
+                <Text mb={4}>
+                  {book?.duration} menit
+              </Text>
+              </FormControl>
+
+              <FormControl id="audio">
+              <FormLabel className="file-upload form-label" htmlFor="audio">
+                Audio
+              </FormLabel>
+                <Text mb={4}>
+                  {book?.audioPath}
+              </Text>
+              </FormControl>
+              
+            </FormControl>
+          </Flex>
+        </Flex>
+      </Center>
+    </>
   );
-}
+};
 
 export default BookDetail;
