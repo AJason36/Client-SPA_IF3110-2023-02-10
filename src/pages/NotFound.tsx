bebc032d9bfbc6d83444528339d54ab6f8165240
Alexander Jason <alexanderjason526@gmail.com>
2023-11-16 21:36:07 +0700

    feat: add not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,6 @@
 import { Box, Center, Image, Text, Link } from '@chakra-ui/react';
 import logo from "../assets/logo_colored.png";
+import { getRole } from '../utils/auth';
 
 const NotFoundPage: React.FC = () => {
   return (
@@ -9,6 +10,17 @@ const NotFoundPage: React.FC = () => {
           <Image src={logo} alt="Audibook Logo" mb="1.5rem" mx="auto" />
           <Text className="main-text" fontWeight="bold" mb="1rem">
             Hi, we couldn't find the page that you were looking for!
+          </Text>
+          <Text>
+            Go back to the{' '}
+            {getRole() === "author" ?
+            <Link href="/../my-book" color="blue.500" mb={4}>
+              Home page
+            </Link> :
+            <Link href="/../collection" color="blue.500" mb={4}>
+            Home page
+          </Link>
+            }
           </Text>
         </Box>
       </Center>
