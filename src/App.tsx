fcb487768238846c7ace22946c9d8617d0bebef8
Alexander Jason <alexanderjason526@gmail.com>
2023-11-16 15:56:32 +0700

    feat: add protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,21 @@
 import * as React from "react"
 import "./index.css"
-// import { colors } from "./global"
-// import {
-//   ChakraProvider,
-//   Box,
-//   Text,
-//   Link,
-//   VStack,
-//   Code,
-//   Grid,
-//   theme,
-// } from "@chakra-ui/react"
-// import { ColorModeSwitcher } from "./ColorModeSwitcher"
-// import { Logo } from "./Logo"
 import { Routes, Route, Navigate } from "react-router-dom";
 import AddBookPage from "./pages/AddBookPage"
 import BookList from "./pages/MyBookList"
-import { CustomRouteProps } from "./types/route";
+import { RouteProps } from "./types/route";
 import LoginPage from "./pages/Login";
 import RegisterPage from "./pages/Register";
 import BookDetail from "./pages/BookDetail";
 import PremiumBookList from "./pages/PremiumBookList";
 import SubscriptionRequestPage from "./pages/SubscriptionRequest";
 import CollectionList from "./pages/CollectionList";
-
-const ProtectedRoute = ({
-  redirectPath = "/notfound",
-  path,
-  children,
-  routeType,
-}: CustomRouteProps): JSX.Element => {
-  return (
-    <div>
-      </div>
-  );
-};
+import { JsxEmit } from "typescript";
+import { Payload, getAuthData } from "./utils/auth";
+import NotFound from "./pages/NotFound";
+import UnauthorizedPage from "./pages/Unauthorized";
+import MyBookList from "./pages/MyBookList";
+import { get } from "http";
 
 interface Book {
   book_id: number;
@@ -70,29 +51,124 @@ const sampleUsers: User[] = [
   { user_id: 3, username: 'Alice' },
 ];
 
+interface BookInfo {
+  title: string;
+  publisher: string;
+  year: number;
+  genre: string;
+  text: string;
+  audio: string;
+}
+interface Details{
+    details:BookInfo;
+}
 // Sample Data for BookDetail component
-const bookDetail: BookPremium = { book_id: 1, name: 'Action', author: 'John Doe' };
+const bookDetail: BookInfo = {
+  title: 'Cerita Sang Pembelajar',
+publisher: 'John Doe',
+year: 2017,
+genre: 'Fiksi',
+text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  audio: 'sample_audio.mp3',
+};
 
-export const App = () => (
-  <div className="App">
-    <Routes>
-           {/* Common Routes */}
-      <Route path="/add" element={<AddBookPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
+const info: Details = { details :bookDetail };
 
-      {/* Book Routes */}
-      <Route path="/book" element={<BookList books={sampleBooks} />} />
-      {/* <Route path="/book/:book_id" element={<BookDetail details={bookDetail} />} /> */}
+function App() {
+  const ProtectedRoute = ({
+    redirectPath = "/notfound",
+    unauthPath="/unauthorized",
+    children,
+    routeType,
+  }: RouteProps): JSX.Element => { 
+    if (localStorage.getItem("token") === null) {
+      return (
+        <>
+          <Navigate to="/login" />;
+        </>
+      );
+    } else {
+      let isCurator = false;
+      const payload: Payload = getAuthData();
+      if (payload) {
+        isCurator = payload.role.toString() ==="curator";
+      }
+      if (isCurator === routeType) {
+        if (routeType === false) {
+          if (payload.username) {
+            return children;
+          } else {
+            return <Navigate to={redirectPath} />;
+          }
+        }
+        return children;
+      } else {
+        return (
+          <>
+            <Navigate to={unauthPath} />;
+          </>
+        );
+      }
+    }
+  }
+  return (
+    <div className="App">
+      <Routes>
+        {/* Common Routes */}
+        {!getAuthData().username ?
+          <>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          </>
+        :null}
+        <Route path="/register" element={<RegisterPage />} />
 
-      {/* Premium Book Routes */}
-      <Route path="/premium-book" element={<PremiumBookList books={premiumBooks} />} />
+        {/* Author Routes */}
+        <Route path="/my-book" element={
+          <ProtectedRoute routeType={false} >
+            <MyBookList books={sampleBooks}/>
+          </ProtectedRoute>
+        } />
 
-      {/* Collection Routes */}
-      <Route path="/collection" element={<CollectionList books={premiumBooks} />} />
+        <Route path="/add" element={
+          <ProtectedRoute routeType={false}>
+            <AddBookPage />
+          </ProtectedRoute>
+        } />
 
-      {/* Subscription Request Routes */}
-      <Route path="/subs-request" element={<SubscriptionRequestPage users={sampleUsers} />} />
-    </Routes>
-  </div>
-)
+        <Route path="/book/details" element={
+          <ProtectedRoute routeType={false} >
+            <BookDetail details={bookDetail} />
+          </ProtectedRoute>
+        }/>
+
+        {/* Curator Routes */}
+        {/* Premium Book Routes */}
+        <Route path="/premium-book" element={
+          <ProtectedRoute routeType={true} >
+            <PremiumBookList books={premiumBooks} />
+          </ProtectedRoute>
+        } />
+
+        {/* Collection Routes */}
+        <Route path="/collection" element={
+          <ProtectedRoute routeType={true}>
+            <CollectionList books={premiumBooks} />
+          </ProtectedRoute>
+        } />
+
+        {/* Subscription Request Routes */}
+        <Route path="/subs-request" element={
+          <ProtectedRoute routeType={true}>
+            <SubscriptionRequestPage users={sampleUsers} />
+          </ProtectedRoute>
+        } />
+        
+        <Route path="/*" element={<NotFound />} />
+
+      </Routes>
+    </div >
+  )
+}
+
+export default App;
